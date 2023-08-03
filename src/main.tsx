86f27f479d606b22647687dc3a5c683f53f967f3
Illus1onnn <1161524405@qq.com>
2023-08-03 20:37:10 +0800

    add comment

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    {/* Chakra UI Provider */}
     <ChakraProvider toastOptions={{ defaultOptions: { position: "top-right" } }}>
       <App />
     </ChakraProvider>
