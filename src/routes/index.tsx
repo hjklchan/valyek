d35d257e6cf24bd97c41b261b28acae35d691cde
Illus1onnn <1161524405@qq.com>
2023-08-03 19:09:25 +0800

    create routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+
+/**
+ * appRouter
+ * 
+ * provides routing element components.
+ */
+export const appRouter = createBrowserRouter(
+    createRoutesFromElements(
+        <React.Fragment>
+            {/* Home page */}
+            <Route path="/" element={<>Home</>} />
+            {/* Login page */}
+            <Route path="/login" element={<Login />} />
+            {/* Registration page */}
+            <Route path="/registration" element={<Registration />} />
+            {/* App layout routes */}
+            <Route element={<AppLayout />}>
+                {/* Profile page */}
+                <Route path="/profile" element={<RequireAuth>Profile</RequireAuth>} />
+            </Route>
+            {/* Not found page */}
+            <Route path="/*" element={<>Page Not Found</>} />
+        </React.Fragment>
+    )
+)
