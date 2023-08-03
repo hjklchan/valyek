cda88a86bfec9ad634c47c5b4c1c6685d3ad6a65
Illus1onnn <1161524405@qq.com>
2023-08-03 19:59:40 +0800

    sign-in api request

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -1,4 +1,8 @@
+import { httpPost } from "@/utils/netx/request"
+import { SignInRequest, SignInResponse } from "./index.d";
 
-export const signInApi = async () => {
-    // TODO Request api logic here...
-}
+// signInApi
+// login or sign-in request api
+export const signInApi = async (data: SignInRequest) => {
+    return await httpPost<SignInResponse>("http://localhost:8080/api/auth/login", data);
+};
--- src/api/auth/index.d.ts
+++ src/api/auth/index.d.ts
@@ -1,9 +1,11 @@
 
+// sign-in request structure
 export type SignInRequest = {
     username: string;
     password: string;
 };
 
+// sign-in response structure
 export type SignInResponse = {
     accessToken: string;
     exipreTime: number;
