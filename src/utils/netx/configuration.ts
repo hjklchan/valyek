9627c4af2871914c73293efeb38e0d984b6b4b5b
Lucas chen <lucas.chen@gbo-asia.com>
2023-08-04 12:29:49 +0800

    handing response error to complete

--- src/utils/netx/configuration.ts
+++ src/utils/netx/configuration.ts
@@ -1,7 +1,18 @@
-import { AxiosError } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 import { ResponseType } from "./index.d"
 
-export const configureResponseError = (error: AxiosError<ResponseType<any>>) => {
-    // TODO handle response error logic...
+const handleResponseError = (code: string) => {
+    // TODO here is handle response error
+    if (code == AxiosError.ERR_NETWORK) {
+        // TODO pop-up warning...
+    }
 }
+
+export const configureResponseError = (error: AxiosError<ResponseType<any>>) => {
+    if (error.response) {
+        handleResponseError(error.response.code);
+        return;
+    }
+}
+
 
