0e5ef450bc85119706b21db7178105e6d8c1e04c
Illus1onnn <1161524405@qq.com>
2023-08-08 00:25:37 +0800

    Content styles need to be completed

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,9 +1,8 @@
 import { Navigate } from "react-router-dom";
 
-const RequireAuth = ({ children }: { children: React.ReactNode }) => {
+export default function({ children }: { children: React.ReactNode }): React.ReactNode {
     // TODO Check auth status
     const authenticated = true;
     return authenticated ? children : <Navigate to={"/login"} replace />
 }
 
-export default RequireAuth;
--- src/layouts/components/Content.tsx
+++ src/layouts/components/Content.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 // Content
 export default function({ children }: { children: React.ReactNode }) {
-    return <main>{children}</main>
+    return <main >{children}</main>
 }
