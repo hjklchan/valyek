7025b776b59e9c5737d03131308d99efaea2196b
Illus1onnn <1161524405@qq.com>
2023-08-14 23:43:17 +0800

    文章详情布局

--- src/pages/post/detail.tsx
+++ src/pages/post/detail.tsx
@@ -3,9 +3,11 @@ import { useParams } from "react-router-dom";
 const PostDetail = () => {
     const { postId } = useParams();
     return <>
-        {/** Post Main Content **/}
-        <article className="prose prose-gray">
-            <h1>Garlic bread with cheese: What the science tells us</h1>
+        {/** Article Title **/}
+        <h1 className="text-3xl font-bold">Garlic bread with cheese: What the science tells us</h1>
+        <br />
+        {/** Article Main Content **/}
+        <article className="prose prose max-w-none min-h-50 hover:prose-a:text-blue-500 prose-neutral">
             <p>
                 For years parents have espoused the health benefits of eating garlic bread with cheese to their
                 children, with the food earning such an iconic status in our culture that kids will often dress
@@ -16,6 +18,10 @@ const PostDetail = () => {
                 springing up around the country.
             </p>
         </article>
+        <div className="my-5">
+            <h3 className="bg-gray-200 text-md px-1">Comment(s)</h3>
+            {/** Article Comment **/}
+        </div>
     </>
 }
 
