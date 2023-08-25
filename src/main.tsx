5dcd6470b83e550b89577acff55a6a7132ca76e4
Illus1onnn <1161524405@qq.com>
2023-08-25 18:50:46 +0800

    全局设置 borderRadius 为 0

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 // reset css
 import "normalize.css";
 import "./index.css";
 import App from './App';
 
-
+const theme = extendTheme({
+    radii: {
+        none: '0',
+        sm: '0',
+        base: '0',
+        md: '0',
+        lg: '0',
+        xl: '0',
+        "2xl": '0',
+        "3xl": '0',
+        full: '0',
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         {/* Chakra UI Provider */}
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <App />
         </ChakraProvider>
     </React.StrictMode>,
